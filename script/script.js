9d2086e6bcd9e0515a0e4e6dd0b80a983bb062f1
masudparvez699 <almasudi699@gmail.com>
2025-08-29 22:46:16 +0600

    finished the design and also made the call function work

---
+++ script/script.js
@@ -0,0 +1,31 @@
+// Call Button Funtionality
+let callButtons = document.getElementsByClassName('call-buttons');
+let coins = parseInt(document.getElementById('coin-count').innerText);
+for (let btns of callButtons) {
+    btns.addEventListener('click', function () {
+        if (coins < 20) {
+            alert("⚔️ Insufficient coins. A minimum of 20 coins is required to make a call.")
+        }
+        else {
+            let name = btns.parentNode.parentNode.children[1].innerText;
+            let number = btns.parentNode.parentNode.children[3].innerText;
+            let time = new Date().toLocaleTimeString();
+            alert("Calling " + name + " " + number + "...")
+            coins = coins - 20;
+            document.getElementById('coin-count').innerText = coins;
+            let container = document.getElementById('calling-history');
+            console.log(container)
+            let div = document.createElement('div');
+            div.innerHTML = `
+            <div class="flex justify-between items-center p-2 bg-gray-50 rounded-xl mb-2">
+                    <div>
+                        <h3 class="font-bold text-xs">${name}</h3>
+                        <p class="text-sm mt-2 text-gray-500">${number}</p>
+                    </div>
+                    <p class="text-sm  text-gray-600">${time}</p>
+                </div> 
+            `;
+            container.appendChild(div);
+        }
+    })
+}
